990182448d908f4a83ff4f929ea612e0b96ff229
Fabian Payan <f.payan@uniandes.edu.co>
2024-04-08 13:02:34 -0500

    Se eliminan codigos de errores innecesarios.

--- src/errors/errors.py
+++ src/errors/errors.py
@@ -10,7 +10,7 @@ class EmailBadRequestError(ApiError):
 
 class EmailAlreadyExistsError(ApiError):
     code = 412
-    description = "El correo ya está registrado"
+    description = "El email ya está registrado"
 
 
 class EmailNotFoundError(ApiError):
@@ -18,26 +18,10 @@ class EmailNotFoundError(ApiError):
     # Usuario no encontrado
     description = "Email no encontrado"
 
-
-class TokenNotFoundError(ApiError):
-    code = 403
-    # El token no está en el encabezado de la solicitud.
-    description = "El token no está en el encabezado de la solicitud."
-
-
-class TokenNotValidError(ApiError):
-    code = 401
-    # El token no es válido o está vencido.
-    description = "El token no es válido o está vencido."
-
-
 class ResetBlackListDBError(ApiError):
     code = 500
     #'Error al eliminar registros de blacklist.
     description = "Error al eliminar registros de blacklist"
 
-class ApiErrorCallBack(ApiError):
-    code = 500
-    description = "Error al obtener callback de TrueNative API"
 
     
